0ecce2650a83db507506210810cd2a79aba5beaa
AndCumm <160231978+AndCumm@users.noreply.github.com>
2026-01-05 06:06:42 +0100

    Add database configuration and column mappings

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+DB_CONFIG = {
+    "PROJECTS": "277d106b79a98036afe5d30550e5a795",
+    "TASKS":    "277d106b79a980149979fd29a39c5ac3",
+    "AREAS":    "277d106b79a9804e80cbceb74d0d9c68",
+    "RESOURCES":"277d106b79a980e1941ad6756debaa9c"
+}
+
+COLUMNS = {
+    "PROJECT_TITLE": "Project Name",
+    "TASK_TITLE": "Task Name",
+    "RELATION_PROJ_AREA": "Area",
+    "RELATION_TASK_PROJ": "Project",
+    "EPIC_SELECT": "Epic"
+}
